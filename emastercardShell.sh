d47afd7fb375a6e8555c3584cc206039d1f28b88
DoxDevOps <73119561+DoxDevOps@users.noreply.github.com>
2020-10-29 08:19:47 +0200

    Update code

--- emastercardShell.sh
+++ emastercardShell.sh
@@ -13,9 +13,9 @@ DIRECTORY="/var/www/emastercard-upgrade-automation"
 #*******************MAY YOU CHECK THE CODE FROM HERE********************************
 if [ ! -d "$DIRECTORY" ];
 then
-  mkdir $DIRECTORY
+    mkdir /var/www && chmod 777 /var/www
+    mkdir $DIRECTORY
     echo "$DIRECTORY has been created for fresh emastercard installation"
-    chmod 777 /var/www
     cd var/www
     echo "Now cloning https://github.com/HISMalawi/emastercard-upgrade-automation"
 
@@ -23,19 +23,17 @@ then
 
     echo "logging into api folder"
     cd $DIRECTORY/api
-    cp api-config.yml.example api-config.yml
-    cp migration-config.yml.example migration-config.yml
-    ./setup.py
+    cp $DIRECTORY/api/api-config.yml.example $DIRECTORY/api/api-config.yml
+    cp $DIRECTORY/api/migration-config.yml.example $DIRECTORY/api/migration-config.yml
+    ./setup.py  $DIRECTORY
 
     sudo docker-compose exec api initialize_database.sh
 else
 
-echo "the folder is there ...Yyyyy"
-  PWD=/var/www/emastercard-upgrade-automation
-
-  		cd ~
-  		cd $PWD && ls
-
-
-
+echo "The project is already installed ... NOW preparing to update !!"
+git describe --tags $DIRECTORY
+      TAG=$(git describe --tags `git rev-list --tags --max-count=1`)
+      echo $TAG $DIRECTORY
+      git checkout $TAG -b latest $DIRECTORY
+      ./setup.py  $DIRECTORY
 fi
