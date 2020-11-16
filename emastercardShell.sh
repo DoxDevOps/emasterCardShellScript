34add1ad9edced34b53f1b13651e54ca29443da2
DoxDevOps <73119561+DoxDevOps@users.noreply.github.com>
2020-11-16 09:11:19 +0200

    Adding comments to script

--- emastercardShell.sh
+++ emastercardShell.sh
@@ -1,25 +1,35 @@
 #!/bin/sh
  
-#The following is the installation guide for E-mastercard
+# The following is the installation guide for eMC
+# The script considers two situations - 1. a site that already has eMC, 2. a new site without eMC
+
+# Make sure you are on ~ 
 cd ~
 echo "Logging into Documents folder"
-#cd /var
 
+# Define dir where the system sits
 DIRECTORY="/var/www/emastercard-upgrade-automation"
 
-#The following is for new installation only
+# FOR NEW INSTALLATION
 
-#*******************START WITH CHECKING IF THE SYSTEM IS ALREADY INSTALLED ON A SITE********************************
+# Check if system dir (as defined above) exists
+# The assumption is that if it doesn't exist, then that is a new site
 if [ ! -d "$DIRECTORY" ];
 then
+    # Create www /dir and grant relevant privilages 
     mkdir /var/www && chmod 777 /var/www
+    
+    # Create the dir where the system sits
     mkdir $DIRECTORY
     echo "$DIRECTORY has been created for fresh emastercard installation"
+    
+    # Move into /www and clone the relevant eMC repo
     cd var/www
     echo "Now cloning https://github.com/HISMalawi/emastercard-upgrade-automation"
-
     git clone https://github.com/HISMalawi/emastercard-upgrade-automation ~/var/www
-
+    
+    # in /api dir change the api-config.yml.example to api-config.yml
+    # in /api dir change migration-config.yml.example to migration-config.yml
     echo "logging into api folder"
     cd $DIRECTORY/api
     cp $DIRECTORY/api/api-config.yml.example $DIRECTORY/api/api-config.yml
@@ -28,15 +38,15 @@ then
 
     sudo docker-compose exec api initialize_database.sh
     
-#*******************ELSE IF THE SYSTEM IS ALREADY UPDATED THEN JUST UPDATE TO THE LATEST TAG********************************
-else
+# FOR EXISTING INSTALLATION
 
-      echo "The project is already installed ... NOW preparing to update !!"
+else
+      # Check out the latest TAG  
+      echo "The project is already installed ... NOW preparing to update!!"
       git --git-dir=/var/www/emastercard-upgrade-automation/.git describe
       TAG=$(git  --git-dir=/var/www/emastercard-upgrade-automation/.git describe --tags `git rev-list --tags --max-count=1`)
       echo $TAG $DIRECTORY
       git --git-dir=/var/www/emastercard-upgrade-automation/.git checkout  $TAG -f
-
       cd /var/www/emastercard-upgrade-automation/ | echo 123456 | sudo -S ./setup.py
-      echo " THE SYSTEM HAS BEEN SUCCESSIFULLY UPDATED !! "
+      echo "The system has successfully been updated."
 fi
